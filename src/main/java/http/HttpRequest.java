cb592ce903db51ad2186243e4ad6abc8a0ddb416
Asgar_George <asgarantony@gmail.com>
2024-09-10 20:21:00 +0530

    working on error code

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -1,10 +1,18 @@
 package http;
 
 public class HttpRequest extends HttpMessage{
-    private String method;
+    private HttpMethod method;
     private String requestTarget;
     private String httpVersion;
     HttpRequest(){
 
+    }
+
+    public HttpMethod getMethod() {
+        return method;
+    }
+
+    void setMethod(HttpMethod method) {
+        this.method = method;
     }
 }
