4c45f4af147b8c761de923cecf1f8cf7f7b3368a
Asgar_George <asgarantony@gmail.com>
2024-09-11 22:34:41 +0530

    working on error code

--- src/main/java/http/HttpParser.java
+++ src/main/java/http/HttpParser.java
@@ -11,30 +11,45 @@ import java.nio.charset.StandardCharsets;
 public class HttpParser {
    private final static Logger LOGGER = LoggerFactory.getLogger(HttpParser.class);
 
-   private static final int SP = 0x20;  //32
-   private static final int CR = 0x0D;  //13
-   private static final int LF = 0x0A;  //10
+   private static final int SP = 0x20;  //32 space
+   private static final int CR = 0x0D;  //13 carriage return
+   private static final int LF = 0x0A;  //10  next line
 
    public HttpRequest parseHttpRequest(InputStream inputStream){
       InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.US_ASCII);
       HttpRequest request = new HttpRequest();
 
-      parseRequestLine(reader,request);
-      parseHeaders(reader,request);
+       try {
+           parseRequestLine(reader,request);
+       } catch (IOException e) {
+           throw new RuntimeException(e);
+       }
+       parseHeaders(reader,request);
       parseBody(reader,request);
       return request;
    }
 
    private void parseRequestLine(InputStreamReader reader, HttpRequest request) throws IOException {
+      StringBuilder processingDataBuffer = new StringBuilder();
       int _byte;
       while ((_byte = reader.read()) >= 9) {
       if (_byte == CR) {
          _byte = reader.read();
 
          if (_byte == LF) {
+            LOGGER.debug("Request Line to Process : {}", processingDataBuffer.toString());
+            return;
          }
       }
+         if(_byte == SP) {
+            //logi
+            LOGGER.debug("Request Line to Process : {}", processingDataBuffer.toString());
+            processingDataBuffer.delete(0, processingDataBuffer.length());
+         }else{
+            processingDataBuffer.append((char) _byte);
+         }
       }
+
    }
 
    private void parseHeaders(InputStreamReader reader, HttpRequest request) {
